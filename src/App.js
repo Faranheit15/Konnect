ab9846e37c95ac5f19d0ca315f61fbe2f0928705
Faranheit15 <ffaranm15@gmail.com>
2021-06-03 11:27:49 +0530

    centering successful

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     // BEM Naming convention
     <div className="App">
-      <h1>Konnect - A Text Messaging App</h1>
+      {/* <h1>Konnect - A Text Messaging App</h1> */}
       <div className="app__body">
         {/* Sidebar */}
         {/* Chat */}
