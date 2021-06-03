cf17a38179b9220ceb25e0413219b279b83b70d8
Faranheit15 <ffaranm15@gmail.com>
2021-06-03 11:26:40 +0530

    divide UI into sidebar and char section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import "./App.css";
 
 function App() {
   return (
+    // BEM Naming convention
     <div className="App">
       <h1>Konnect - A Text Messaging App</h1>
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
     </div>
   );
 }
