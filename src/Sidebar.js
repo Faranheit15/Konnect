58cb026e34244c704e5924d9e20ad4fbc8d07751
Faranheit15 <ffaranm15@gmail.com>
2021-06-06 03:49:25 +0530

    sidebar__header complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,29 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLarge from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import React from 'react'
 import './Sidebar.css';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLarge />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search"></div>
+            <div className="sidebar__chats"></div>
         </div>
     )
 }
