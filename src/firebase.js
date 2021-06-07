900c897ad33c638e71bcf5dc0708757e83f5a92f
Faranheit15 <ffaranm15@gmail.com>
2021-06-07 14:36:30 +0530

    change folder structure and add firebaseConfig

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDWQElYDkyT7fSn9z9i4kyQAX5UBQbhIps",
+    authDomain: "konnect-3f625.firebaseapp.com",
+    projectId: "konnect-3f625",
+    storageBucket: "konnect-3f625.appspot.com",
+    messagingSenderId: "987795163062",
+    appId: "1:987795163062:web:59f7a061f07a7b44ede317",
+    measurementId: "G-ZKE33W2GCE"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
