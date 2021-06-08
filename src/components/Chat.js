9889d2b41f3ea4411fbac000fbc8d2d19fac7c62
Faranheit15 <ffaranm15@gmail.com>
2021-06-08 18:58:24 +0530

    authentication and context API start

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,14 +4,24 @@ import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React, { useEffect, useState } from 'react';
 import "./Chat.css"
+import { useParams } from 'react-router-dom';
+import db from '../firebase';
 
 function Chat() {
     const [input, setInput] = useState('');
     const [seed, setSeed] = useState('');
+    const { roomId } = useParams();
+    const [roomName, setRoomName] = useState('');
+
+    useEffect(() => {
+        if(roomId){
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => (setRoomName(snapshot.data().name)))
+        }
+    }, [roomId])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000))
-    }, [])
+    }, [roomId])
 
     const sendMessage = (e) => {
         e.preventDefault();
@@ -24,7 +34,7 @@ function Chat() {
             <div className="chat__header">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
                 <div className="chat__headerInfo">
-                    <h3>Room name</h3>
+                    <h3>{roomName}</h3>
                     <p>Last seen at ...</p>
                 </div>
                 <div className="chat__headerRight">
--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,8 +1,10 @@
 import { Avatar } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
+import db from '../firebase';
+import { Link } from 'react-router-dom';
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ id, name, addNewChat }) {
     const [seed, setSeed] = useState('');
 
     useEffect(() => {
@@ -10,20 +12,26 @@ function SidebarChat({ addNewChat }) {
     }, [])
 
     const createChat = () =>{
-        const roomName = prompt("Please enter name for chat");
+        const roomName = prompt("Please enter name for chat room");
         if (roomName){
             //do some clever stuff in the database
+            db.collection('rooms').add({
+                name: roomName,
+            });
         }
     };
 
     return !addNewChat ? (
-        <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-            <div className="sidebarChat__info">
-                <h2>Room name</h2>
-                <p>Last message...</p>
+        <Link to = {`/rooms/${id}`}>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>Last message...</p>
+                </div>
             </div>
-        </div>
+        </Link>
+        
     ) : (
         <div onClick={createChat}
         className="sidebarChat">
