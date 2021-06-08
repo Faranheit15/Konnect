2f474f576d90bb3ef74477105e33ab5981b0bc3a
Faranheit15 <ffaranm15@gmail.com>
2021-06-09 00:05:03 +0530

    authentication complete

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,9 +7,11 @@ import './Sidebar.css';
 import { SearchOutlined } from '@material-ui/icons';
 import SidebarChat from './SidebarChat';
 import db from '../firebase';
+import { useStateValue } from '../StateProvider';
 
 function Sidebar() {
     const [rooms, setRooms] = useState([]);
+    const [{ user }, dispatch] = useStateValue();
 
     useEffect(() => {
         const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
@@ -28,7 +30,7 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar />
+                <Avatar src={user?.photoURL}/>
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLarge />
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import reducer,{initialState} from './reducer';
-import {StateProvider} from './StateProvider';
+import reducer, { initialState } from './reducer';
+import { StateProvider } from './StateProvider';
 
 ReactDOM.render(
   <React.StrictMode>
